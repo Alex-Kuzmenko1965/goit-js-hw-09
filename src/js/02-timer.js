a8e698660ea2b29dc4faba81f83ee772298e622e
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-04-05 16:24:16 +0300

    Alex

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+  input: inputEl = document.getElementById('datetime-picker'),
+  start: startBtn = document.querySelector('button'),
+};
+
+console.log(inputEl);
+console.log(startBtn);
